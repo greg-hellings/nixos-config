848b61d7ada7e25d9a39558b2e2098ace5b5c7c8
Greg Hellings <greg.hellings@gmail.com>
2022-04-13 06:51:26 +0000

    Allow read permissions on backups

--- hosts/linode/postgres.nix
+++ hosts/linode/postgres.nix
@@ -44,4 +44,8 @@ root root postgres
 		enable = true;
 		devices = [ "nas" ];
 	};
+
+	services.cron.systemCronJobs = [
+		"59 2 * * * root chmod -R a+r ${config.services.postgresqlBackup.location} && find ${config.services.postgresqlBackup.location} -type d -exec chmod a+x '{}' \\;"
+	];
 }
