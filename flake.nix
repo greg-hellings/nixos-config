eb17fb1d3c993368d5fe4b8125f1268c30ce5be1
Greg Hellings <greg.hellings@gmail.com>
2022-06-02 08:20:45 -0500

    Upgrade to 22.05

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
 	description = "Greg's machines!";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
 		nixunstable.url = "github:nixos/nixpkgs/nixos-unstable";
 		agenix.url = "github:ryantm/agenix";
 		home-manager = {
-			url = "github:nix-community/home-manager/release-21.11";
+			url = "github:nix-community/home-manager/release-22.05";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		nur.url = "github:nix-community/NUR";
