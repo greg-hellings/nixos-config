36e370fb73d2f6eccd340c72e9f3866ff304fee3
Greg Hellings <greg.hellings@gmail.com>
2022-08-13 00:42:30 -0500

    Default configuration.nix file

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./profiles/nix.nix
+      ./modules/automatic/nix.nix
     ];
 
   # Bootloader.
