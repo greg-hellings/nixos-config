efcece0c69d6f08dd7ab7ea1af1ab2aaab5a970a
greg-compass <gregory.hellings@compass.com>
2022-09-09 14:36:35 -0500

    Fix datadog license

--- overlays/datadog-api-client.nix
+++ overlays/datadog-api-client.nix
@@ -6,13 +6,13 @@ buildPythonPackage rec {
 
 	src = fetchPypi {
 		inherit pname version;
-		sha256 = "";
+		sha256 = "CIEQlw8S2lZk1n+ld/Ne7lBx4SKaJZUZfNbw9KvUcjk=";
 	};
 
 	meta = with lib; {
 		description = "Datadog API client support";
 		homepage = "https://github.com/DataDog/datadog-api-client-python";
-		license = licenses.bsd;
+		license = licenses.asl20;
 		maintainers = [];
 	};
 
