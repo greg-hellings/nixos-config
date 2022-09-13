538d08a2e8c26eb55e6527d37cbf3573f1f453e0
greg-compass <gregory.hellings@compass.com>
2022-09-13 16:34:22 -0500

    Add android file transfer for file transfers on Android

--- modules-all/automatic/programs.nix
+++ modules-all/automatic/programs.nix
@@ -19,6 +19,7 @@ in {
 	# Base packages that need to be in all my hosts
 	environment.systemPackages = with pkgs; [
 		agenix.defaultPackage."${system}"
+		android-file-transfer
 		bitwarden-cli
 		diffutils
 		git
