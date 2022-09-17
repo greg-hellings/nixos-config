20537b886467c70478a55cf7fbcb0e39fdd867fa
Greg Hellings <greg.hellings@gmail.com>
2022-09-17 11:30:52 -0500

    Add multi-account containers

--- home/gui/firefox.nix
+++ home/gui/firefox.nix
@@ -17,6 +17,7 @@ in with lib;
 		package = ffPkgs;
 		extensions = with pkgs.nur.repos.rycee.firefox-addons; [
 			bitwarden
+			multi-account-containers
 			octotree
 			refined-github
 			tree-style-tab
