766ef2ff2c8f382981e2b48f73dedd3bd1a32d32
greg-compass <gregory.hellings@compass.com>
2022-11-30 11:13:10 -0600

    Remove VSCode

--- home/gui/default.nix
+++ home/gui/default.nix
@@ -33,44 +33,4 @@
 			nextcloud-client
 		] else []
 	);
-
-	programs = {
-		vscode = let
-			codepkg = pkgs.vscode-with-extensions.override {
-				vscodeExtensions = with pkgs.vscode-extensions; [
-					eamodio.gitlens
-					file-icons.file-icons
-					formulahendry.auto-rename-tag
-					github.vscode-pull-request-github
-					golang.go
-					johnpapa.vscode-peacock
-					jnoortheen.nix-ide
-					kamikillerto.vscode-colorize
-					ms-azuretools.vscode-docker
-					ms-python.python
-					redhat.java
-					redhat.vscode-yaml
-					ritwickdey.liveserver
-					streetsidesoftware.code-spell-checker
-					vscodevim.vim
-					yzhang.markdown-all-in-one
-				] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-					{
-						name = "vscode-bazel";
-						publisher = "BazelBuild";
-						version = "0.5.0";
-						sha256 = "sha256-JJQSwU3B5C2exENdNsWEcxFSgWHnImYas4t/KLsgTj4=";
-					}
-				];
-			};
-		in {
-			enable = true;
-			# userSettings = {
-			# 	"update.channel" = "none";
-			# 	"editor.renderWhitespace" = "all";
-			# 	"editor.tabWidth" = "8";
-			# };
-			package = codepkg;
-		};
-	};
 }
