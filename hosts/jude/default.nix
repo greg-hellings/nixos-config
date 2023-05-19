45212088416c2d696b987e09a30197063d8d3e7b
Greg Hellings <greg.hellings@gmail.com>
2023-05-19 08:56:51 -0500

    Switch back to Gnome

--- hosts/jude/default.nix
+++ hosts/jude/default.nix
@@ -11,7 +11,8 @@
 	programs.steam.enable = true;
 	networking.hostName = "jude";
 	networking.enableIPv6 = false;
+	#systemd.oomd.enable = false;
 	greg.tailscale.enable = true;
-	greg.gnome.enable = false;
-	greg.kde.enable = true;
+	greg.gnome.enable = true;
+	greg.kde.enable = false;
 }
