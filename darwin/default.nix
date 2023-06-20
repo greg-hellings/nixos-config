e6ef56013b04ee87ff9e72b32603152ec5ed76f7
Greg Hellings <greg.hellings@gmail.com>
2023-06-20 15:55:02 -0500

    Auto-gen home-manager with system

--- darwin/default.nix
+++ darwin/default.nix
@@ -8,7 +8,7 @@ let
 	}:
 	let
 		nixpkgs = import channel {
-			inherit system;
+			inherit system overlays;
 		};
 	in darwin.lib.darwinSystem {
 		inherit system;
@@ -18,13 +18,16 @@ let
 			agenix.nixosModules.default
 			hm.darwinModules.home-manager
 			{
-				home-manager.useGlobalPkgs = true;
-				home-manager.useUserPackages = true;
-				home-manager.users."gregory.hellings" = import ../home/home.nix;
-				home-manager.extraSpecialArgs = {
-					gnome = false;
-					gui = true;
+				home-manager = {
+					useGlobalPkgs = true;
+					users."gregory.hellings" = import ../home/home.nix;
+					extraSpecialArgs = {
+						gnome = false;
+						gui = true;
+						home = "/Users/gregory.hellings";
+					};
 				};
+				users.users."gregory.hellings".home = "/Users/gregory.hellings";
 			}
 			../modules-all
 			../modules-darwin
@@ -32,7 +35,11 @@ let
 		] ++ extraMods;
 	};
 in rec {
-	"C02G48H8MD6R" = mac { system = "x86_64-darwin"; name = "work"; };
+	# Duplicate all the things, because nix-darwin does different things with case sensitivity
+	# depending on where you live
+	C02G48H8MD6R = mac { system = "x86_64-darwin"; name = "work"; };
+	c02g48h8md6r = C02G48H8MD6R;
+
 	la23002 = mac { name = "ivr"; };
 	LA23002 = la23002;
 }
--- home/home.nix
+++ home/home.nix
@@ -1,4 +1,5 @@
-{ pkgs, lib, gui, gnome, ...}:
+{ pkgs, lib, gui, gnome,
+  ...}:
 
 {
 	imports = [
