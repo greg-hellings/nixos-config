ab47464200da0f6168d8a04f4aca1389a978f7f4
Greg Hellings <greg.hellings@gmail.com>
2023-08-09 12:34:50 -0500

    Try to improve Windows support

--- hosts/jude/boot.nix
+++ hosts/jude/boot.nix
@@ -4,10 +4,12 @@
 	# Use the systemd-boot EFI boot loader.
 	#boot.loader.systemd-boot.enable = true;
 	boot.loader.grub.device = "/dev/nvme0n1";
-	boot.loader.grub.useOSProber = true;
+	boot.loader.grub.useOSProber = false;
 	boot.loader.grub.extraEntries = ''
-		menuentry "Windows" {
-		  chainloader (hd0,2)+1
+		menuentry "Windows" --class windows --class os {
+		  insmod nfts
+		  search --no-floppy --set=root --fs-uuid A41B-5963
+		  chainloader (${root})/EFI/Microsoft/Boot/bootmgfw.efi
 		}
 	'';
 	boot.loader.efi.canTouchEfiVariables = true;
