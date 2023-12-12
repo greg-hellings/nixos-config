d807217493740e0075f8172ac493ea6f2dcf6042
Greg Hellings <greg.hellings@gmail.com>
2023-12-12 14:28:07 -0600

    Update ssh alias

--- home/ssh.nix
+++ home/ssh.nix
@@ -27,7 +27,7 @@
 			"chronicles.thehellings.lan" = lib.hm.dag.entryBefore [ "*.thehellings.lan"] nas;
 
 			gh = { user = "git"; hostname = "github.com"; };
-			"src" = { user = "gitea"; hostname = "src.thehellings.com"; };
+			"src" = { user = "forgejo"; hostname = "src.thehellings.com"; };
 
 			"*.thehellings.lan" = owned;
 			"10.42.*" = owned;
