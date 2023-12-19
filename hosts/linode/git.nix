714f9a6525f039dd0a62663a9223749c94cb65f8
Greg Hellings <greg.hellings@gmail.com>
2023-12-19 20:08:22 +0000

    Add HAProxy for Git SSH

--- hosts/linode/git.nix
+++ hosts/linode/git.nix
@@ -12,6 +12,30 @@ in {
 		proxy_set_header X-Forwarded-Ssl on;
 		'';
 	};
+
+	services.haproxy = {
+		enable = true;
+		config = builtins.concatStringsSep "\n" [
+			"global"
+			"	daemon"
+			"	maxconn 20"
+			
+			"defaults"
+			"	timeout connect 500s"
+			"	timeout client 500s"
+			"	timeout server 1h"
+
+			"frontend gitsshd"
+			"	bind *:2222"
+			"	default_backend gitssh"
+			"	timeout client 1h"
+
+			"backend gitssh"
+			"	mode tcp"
+			"	server git-thehellings-lan git.thehellings.lan:2222"
+		];
+	};
+
 	##########################################################################################
 	###########
 	#                       CI SERVICES
