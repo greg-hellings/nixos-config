ab39520f23c9857265189d88a6bd18891fc5cd00
Greg Hellings <greg.hellings@gmail.com>
2024-02-26 15:42:34 -0600

    More options in Sway

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -2,18 +2,29 @@
 
 let
 	cfg = config.greg.sway;
+	file_browser = {
+		pkg = pkgs.krusader;
+		path = "${pkgs.krusader}/bin/krusader";
+	};
 in {
 	options.greg.sway = lib.mkEnableOption "Enable Sway support and settings";
 
 	config = (lib.mkIf cfg {
 		programs.swaylock.enable = true;
 
-		wayland.windowManager.sway = {
+		wayland.windowManager.sway = let
+			mod = config.wayland.windowManager.sway.config.modifier;
+		in {
 			enable = true;
 			config = rec {
 				#fonts.size = 10.0;
 				keybindings = lib.mkOptionDefault {
-					"Mod3+l" = "${pkgs.swaylock}/bin/swaylock";
+					"Mod4+l" = "exec ${pkgs.swaylock}/bin/swaylock -c 000000";
+					"Mod4+h" = "exec ${pkgs.qpwgraph}/bin/qpwgraph -x /home/greg/sound/headphones.qpwgraph -m";
+					"Mod4+m" = "exec ${pkgs.qpwgraph}/bin/qpwgraph -x /home/greg/sound/monitor.qpwgraph -m";
+					"Mod4+b" = "exec ${pkgs.qpwgraph}/bin/qpwgraph -x /home/greg/sound/both.qpwgraph -m";
+
+					"${mod}+Shift+Return" = file_browser.path;
 				};
 				modifier = "Mod1";
 				output = {
@@ -61,10 +72,13 @@ in {
 			kate
 			kget
 			krename
-			krusader
+			file_browser.pkg
 			p7zip
 			plocate
 			rpm
+			qpwgraph
+			xorg.xev
+			xorg.xmodmap
 			xxdiff
 		];
 	});
