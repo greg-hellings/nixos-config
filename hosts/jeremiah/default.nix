3798959948bdc5bf76f100239def4e3c0906785e
Greg Hellings <greg.hellings@gmail.com>
2024-05-06 13:30:36 -0500

    Jeremiah with more drives

--- hosts/jeremiah/default.nix
+++ hosts/jeremiah/default.nix
@@ -1,22 +1,38 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# your system.	Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+		];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# Bootloader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "jeremiah"; # Define your hostname.
-  greg = {
+	networking.hostName = "jeremiah"; # Define your hostname.
+	greg = {
 	home = true;
-	tailscale.enable = true;
-  };
+		tailscale.enable = true;
+	};
+	environment.systemPackages = with pkgs; [
+		btrfs-progs
+	];
+	
+	fileSystems = {
+		"/nix" = {
+			fsType = "btrfs";
+			options = [ "subvol=nix" ];
+			device = "/dev/nvme0n1p1";
+		};
+		"/var" = {
+			fsType = "btrfs";
+			options = [ "subvol=var" ];
+			device = "/dev/nvme0n1p1";
+		};
+	};
 }
