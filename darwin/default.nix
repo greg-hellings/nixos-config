0317d03ce2f1dde05b682c28034a528b0c42c0f0
Greg Hellings <greg.hellings@gmail.com>
2024-08-04 20:30:49 -0500

    Remove Compass references

--- darwin/default.nix
+++ darwin/default.nix
@@ -28,11 +28,6 @@ let
 		] ++ extraMods;
 	};
 in rec {
-	# Duplicate all the things, because nix-darwin does different things with case sensitivity
-	# depending on where you live
-	C02G48H8MD6R = mac { system = "x86_64-darwin"; name = "work"; };
-	c02g48h8md6r = C02G48H8MD6R;
-
 	la23002 = mac { name = "ivr"; };
 	LA23002 = la23002;
 }
