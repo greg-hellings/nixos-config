ca918f7bbdd950ab95b38351b50e8e6edf08b917
Greg Hellings <greg.hellings@gmail.com>
2024-10-10 10:41:37 -0500

    Add binary caching for this

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
 {
   description = "Greg's machines!";
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g" ];
+  };
+
   inputs = {
     agenix = {
       url = "github:ryantm/agenix";
