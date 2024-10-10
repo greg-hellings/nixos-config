38899afeb308d2b9bba5ef15b41fada7908683e8
Greg Hellings <greg.hellings@gmail.com>
2024-10-10 15:09:28 -0500

    Add layout_poetry to direnv
    Give direnv knowledge of how to use Poetry

--- home/modules/baseline/direnv.nix
+++ home/modules/baseline/direnv.nix
@@ -4,5 +4,18 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      layout_poetry() {
+        if [[ ! -f pyproject.toml ]]; then
+          echo "No pyproject.toml found"
+          exit 1
+        fi
+
+        venv="$(dirname "$(poetry run which python)")"
+        export VIRTUAL_ENV="$(echo "$venv" |  rev | cut -d'/' -f2- | rev)"
+        export POETRY_ACTIVE=1
+        PATH_add "$venv"
+      }
+    '';
   };
 }
