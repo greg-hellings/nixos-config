5fddcfcb2d11732e1a504e48205ad67a6a686b1d
Greg Hellings <greg.hellings@gmail.com>
2024-11-04 10:42:50 -0600

    Add genesis key

--- modules/nixos/remote-builder.nix
+++ modules/nixos/remote-builder.nix
@@ -17,6 +17,7 @@ with lib;
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGJjyFVOsF74QKzRITc8z/5MJlIa47P1tMm9Z8HRJLm root@jude"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMMOdWSq6NtcP6sfe2uke4wSfgE16hfa970t+8ADdLwk root@nixos"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwbFyuS9zcwZ7hGSpAWtx9NJq/elSLN8jYpr+/1nXd8 root@linode"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXza2Pj05pdzGbiPDc/RF13nHhIZZ63u/WV/BS7bOjf root@genesis"
       ];
       homeMode = "500";
       isNormalUser = true;
