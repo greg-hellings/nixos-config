3a673594c3972127b749d49485fbe303e9863b14
Greg Hellings <greg.hellings@gmail.com>
2025-04-17 16:59:05 -0500

    Bump nextcloud to 31

--- hosts/linode/nextcloud.nix
+++ hosts/linode/nextcloud.nix
@@ -6,7 +6,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     appstoreEnable = true;
     hostName = "next.${config.networking.domain}";
     https = true;
