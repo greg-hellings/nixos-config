9ffff4adcb2b403b533e9b279dbe7d59eab286cc
Greg Hellings <greg.hellings@gmail.com>
2025-07-11 10:48:17 -0400

    Impala is Linux-only

--- home/baseline/tools.nix
+++ home/baseline/tools.nix
@@ -1,13 +1,14 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
     dig
     dnsutils
     jqp
     iamb
-    impala
     rainfrog
     tenere
     wiki-tui
-  ];
+  ] ++ (lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+    impala
+  ]);
 }
