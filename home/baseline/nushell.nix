a1be0912da0c7285e778db60e35bb92548adf437
Greg Hellings <greg.hellings@gmail.com>
2025-08-13 22:32:54 -0500

    Standardize lots of shell aliases

--- home/baseline/nushell.nix
+++ home/baseline/nushell.nix
@@ -5,25 +5,8 @@
 }:
 {
   programs = {
-    carapace = {
-      enable = true;
-      enableNushellIntegration = true;
-    };
-    direnv = {
-      enable = true;
-      enableNushellIntegration = true;
-    };
     nushell = {
       enable = true;
-      environmentVariables = {
-        AWS_SHARED_CREDENTIALS_FILE = "/run/agenix/cache-credentials";
-        GOPATH = "${config.home.homeDirectory}/src/go";
-        GOBIN = "${config.home.homeDirectory}/src/bin";
-        LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN = "1";
-        SWORD_PATH = "${config.home.homeDirectory}/.sword/";
-        #TIMEFORMAT = "%3Uu %3Ss %3lR %P%%";
-        VIRTUALENV_HOME = "${config.home.homeDirectory}/venv/";
-      };
       extraConfig = ''
         use std/util "path add"
         path add ${config.home.homeDirectory}/src/bin
@@ -35,9 +18,6 @@
         buffer_editor = lib.getExe config.programs.nixvim.package;
       };
     };
-    starship = {
-      enable = true;
-      enableNushellIntegration = true;
-    };
   };
+  home.shell.enableNushellIntegration = true;
 }
--- home/baseline/shell.nix
+++ home/baseline/shell.nix
@@ -1,11 +1,62 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  home = {
+    sessionVariables = {
+      AWS_SHARED_CREDENTIALS_FILE = "/run/agenix/cache-credentials";
+      CARAPACE_BRIDGES = "zsh,bash";
+      EDITOR = "nvim";
+      GOPATH = "${config.home.homeDirectory}/src/go";
+      GOBIN = "${config.home.homeDirectory}/src/bin";
+      LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN = "1";
+      MAVEN_OPTS = " -Dmaven.wagon.http.ssl.insecure=true";
+      SWORD_PATH = "${config.home.homeDirectory}/.sword/";
+      #TIMEFORMAT = "%3Uu %3Ss %3lR %P%%";
+      VIRTUALENV_HOME = "${config.home.homeDirectory}/venv/";
+    };
+    shellAliases = {
+      nb = "nix build -L";
+      nixdu = "sudo nix-store --gc --print-roots | egrep -v \"^(/nix/var|/run/\\\\w+-system|\\\\{memory|/proc)\"";
+      nixtest = "nixpkgs-review rev HEAD";
+      nixup = "nix flake lock update";
+      nixcopy = "nix copy --to \"s3://binary-cache/?profile=default&endpoint=nas.home%3A9000&scheme=http\"";
+      s = "nix run \".#runserver\"";
+      updateScript = "nix-shell maintainers/scripts/update.nix --argstr package";
+
+      # General
+      k = "kubectl";
+      kn = "kubectl get nodes -o wide";
+      kp = "kubectl get pods -o wide";
+      win = "sudo virsh start win10";
+      yaml2js = "python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)";
+      z = "zeditor .";
+
+      # Tailscale related ones
+      tsup = "sudo tailscale up";
+      tspub = "sudo tailscale up --exit-node=linode";
+      tshome = "sudo tailscale up --exit-node=2maccabees";
+      tsclear = "sudo tailscale up --exit-node=''";
+
+      # Vagrant related
+      vdown = "vagrant destroy";
+      vhalt = "vagrant halt";
+      vos = "vagrant up --provision --provider openstack";
+      vprov = "vagrant provision";
+      vup = "vagrant up --provision --provider libvirt";
+      vssh = "vagrant ssh";
+    };
+  };
   programs = {
+    carapace = {
+      enable = true;
+    };
+    direnv = {
+      enable = true;
+    };
     ghostty = {
       enable = true;
       package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
       settings = {
-        command = "~/.nix-profile/bin/xonsh";
+        command = if pkgs.stdenv.hostPlatform.isDarwin then "~/.nix-profile/bin/xonsh" else "nu";
         font-family = "Hacker";
         theme = "Dracula";
         scrollback-limit = "1000000";
@@ -29,6 +80,9 @@
           "ctrl+b>p=previous_tab"
         ];
       };
+    };
+    starship = {
+      enable = true;
     };
   };
 }
