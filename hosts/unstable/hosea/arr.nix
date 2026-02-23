46138b90c237ef2f479e2ade59460cb05c03a0fc
Greg Hellings <greg.hellings@gmail.com>
2026-02-23 17:14:08 -0600

    chore: enable transmission on hosea

---
+++ hosts/unstable/hosea/arr.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  metadata,
+  pkgs,
+  ...
+}:
+let
+  ips = lib.filterAttrs (_k: v: v ? "ip" && v.ip != null) metadata.hosts;
+  tps = lib.filterAttrs (_k: v: v ? "ts" && v.ts != null) metadata.hosts;
+  get = field: set: lib.mapAttrsToList (_k: v: v.${field}) set;
+  getTS = get "ts" tps;
+  getIP = get "ip" ips;
+  whitelists = builtins.concatStringsSep "," (
+    [
+      "localhost"
+      "127.0.0.1"
+    ]
+    ++ getTS
+    ++ getIP
+  );
+in
+{
+  services = {
+    transmission = {
+      enable = true;
+      openPeerPorts = true;
+      openRPCPort = true;
+      package = pkgs.transmission_4;
+      settings = {
+        rpc-bind-address = "0.0.0.0";
+        rpc-host-whitelist = whitelists;
+        rpc-host-whitelist-enabled = false;
+        rpc-whitelist = whitelists;
+        rpc-whitelist-enabled = false;
+        watch-dir-enabled = true;
+        watch-dir = "/arr/transmission/incoming";
+      };
+    };
+  };
+
+  systemd.mounts =
+    let
+      nfs = name: {
+        what = "nas1.shire-zebra.ts.net:/mnt/all/${name}";
+        type = "nfs";
+        name = "${name}.mount";
+        where = "/${name}";
+        requires = [ "tailscaled-autoconnect.service" ];
+        after = [ "tailscaled-autoconnect.service" ];
+        wantedBy = [ "multi-user.target" ];
+        mountConfig.Options = "_netdev,noexec,timeo=50,retrans=5,soft";
+      };
+    in
+    [
+      (nfs "arr")
+      (nfs "music")
+      (nfs "photos")
+      (nfs "video")
+    ];
+}
--- hosts/unstable/hosea/default.nix
+++ hosts/unstable/hosea/default.nix
@@ -17,6 +17,7 @@ in
 {
   imports = [
     # Include the results of the hardware scan.
+    ./arr.nix
     ./hardware-configuration.nix
   ];
 
@@ -141,25 +142,6 @@ in
       variant = "";
     };
   };
-
-  systemd.mounts =
-    let
-      nfs = name: {
-        what = "nas1.shire-zebra.ts.net:/mnt/all/${name}";
-        type = "nfs";
-        name = "${name}.mount";
-        where = "/${name}";
-        requires = [ "tailscaled-autoconnect.service" ];
-        after = [ "tailscaled-autoconnect.service" ];
-        wantedBy = [ "multi-user.target" ];
-        mountConfig.Options = "_netdev,noexec,ro,timeo=50,retrans=5,soft";
-      };
-    in
-    [
-      (nfs "music")
-      (nfs "photos")
-      (nfs "video")
-    ];
 
   users.users = {
     greg.extraGroups = [ "vboxusers" ];
