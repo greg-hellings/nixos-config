15ed4ae5f72f96e1e06cd1e78b41f01f64bd102e
Greg Hellings <greg.hellings@gmail.com>
2026-04-01 15:49:32 -0500

    chore: allow click.discord.com

--- hosts/unstable/genesis/default.nix
+++ hosts/unstable/genesis/default.nix
@@ -17,7 +17,7 @@ let
       curl -s https://adaway.org/hosts.txt | sed '1,24d' | sed 's/127.0.0.1/0.0.0.0/' >> /etc/adblock_hosts
 
       # Custom domains that I need to preserve for some reason
-      for f in "segment.com" "segment.io" "branch.io" "dev.visualwebsiteoptimizer.com"; do
+      for f in "segment.com" "segment.io" "branch.io" "dev.visualwebsiteoptimizer.com" "click.discord.com"; do
        	sed -i -e "/''${f}/d" /etc/adblock_hosts  # Blocks Trelly content for house investors
       done
 
