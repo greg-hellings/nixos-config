05f3f118acdf979eb87bf7aa4251c30e4e4fd66c
Greg Hellings <greg.hellings@gmail.com>
2026-04-06 16:24:53 -0500

    fix: simplify inject-darwin

--- pkgs/inject-darwin.nix
+++ pkgs/inject-darwin.nix
@@ -3,8 +3,8 @@
   coreutils,
   curl,
   gnutar,
+  inetutils,
   nix,
-  nix-output-monitor,
   ...
 }:
 writeShellApplication {
@@ -13,15 +13,15 @@ writeShellApplication {
     coreutils
     curl
     gnutar
+    inetutils
     nix
-    nix-output-monitor
   ];
   text = ''
     # Get my configuration
     cd /etc/nix-darwin
 
     # Build NixOS for this system
-    nom build --extra-experimental-features "nix-command flakes" ".#darwinConfigurations.$(hostname -s).system"
+    nix --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.$(hostname -s).system"
     sudo mv /etc/bashrc /etc/bashrc.before-nix-darwin
     sudo mv /etc/zshrc /etc/zshrc.before-nix-darwin
     sudo ./result/sw/bin/darwin-rebuild switch --flake .
