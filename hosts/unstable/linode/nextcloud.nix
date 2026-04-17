fb4c6966af032414a41ff385dfc95260ccd233a9
Greg Hellings <greg.hellings@gmail.com>
2026-04-17 16:01:36 -0500

    chore: upgrade Nextcloud to 33

--- hosts/unstable/linode/nextcloud.nix
+++ hosts/unstable/linode/nextcloud.nix
@@ -13,7 +13,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
     appstoreEnable = true;
     hostName = "next.${config.networking.domain}";
     https = true;
