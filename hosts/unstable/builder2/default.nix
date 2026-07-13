4f79033b04a2d8cea8596b4e2ec4471cb92ad446
Greg Hellings <greg.hellings@gmail.com>
2026-07-13 18:18:54 -0500

    chore: add Proxmox CT baseline

---
+++ hosts/unstable/builder2/default.nix
@@ -0,0 +1,19 @@
+{ config, modulesPath, pkgs, lib, ... }:
+{
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
+  nix.settings = { sandbox = false; };  
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+  services.fstrim.enable = false; # Let Proxmox host handle fstrim
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+        PermitRootLogin = "yes";
+        PasswordAuthentication = true;
+        PermitEmptyPasswords = "yes";
+    };
+  };
+}
